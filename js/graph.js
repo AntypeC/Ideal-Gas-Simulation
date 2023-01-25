e9517c36bad110a84bb1796c759a7d8ea7a7c1de
AntypeC <3c7975@gmail.com>
2023-01-25 21:14:03 +0800

    update

--- js/graph.js
+++ js/graph.js
@@ -1,37 +1,55 @@
-var xyValues = [
-  {x:50, y:7},
-  {x:60, y:8},
-  {x:70, y:8},
-  {x:80, y:9},
-  {x:90, y:9},
-  {x:100, y:9},
-  {x:110, y:10},
-  {x:120, y:11},
-  {x:130, y:14},
-  {x:140, y:14},
-  {x:150, y:15}
-];
+var ctx = document.getElementById('P-against-V').getContext('2d');
+var n = 1; // number of moles
+var R = 8.314; // ideal gas constant
+var T = [273, 298, 323, 348, 373]; // temperatures in Kelvins
+var P = []; // pressures
 
-new Chart("P-against-V", {
-  type: "scatter",
-  data: {
-    labels: ['P', 'V'],
-    datasets: [{
-      pointRadius: 4,
-      pointBackgroundColor: "rgb(0,0,255)",
-      borderColor: '#FFFFFF',
-      data: xyValues
-    }]
-  },
-  options: {
-    legend: {
-      display: true
+// calculate pressures for each temperature using PV = nRT
+for (var i = 0; i < T.length; i++) {
+    P[i] = n * R * T[i];
+}
+
+new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ['273K', '298K', '323K', '348K', '373K'],
+        datasets: [{
+            label: 'Pressure (P)',
+            data: P,
+            backgroundColor: 'rgba(255, 99, 132, 0.2)',
+            borderColor: 'rgba(255, 99, 132, 1)',
+            borderWidth: 1
+        }]
     },
-    scales: {
-      xAxes: [{ticks: {min: 40, max:160}}],
-      yAxes: [{ticks: {min: 6, max:16}}],
+    options: {
+        scales: {
+          xAxes: [{
+            ticks: {
+              fontColor: 'white'
+            },
+            // Change the color of the x-axis to red
+            gridLines: {
+                color: 'grey'
+            }
+          }],
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            },
+              // Change the color of the y-axis to blue
+              gridLines: {
+                  color: 'grey'
+              }
+          }],
+          yAxes: [{
+              ticks: {
+                  beginAtZero: true
+              }
+          }]
+        },
+        title: {
+            display: true,
+            text: 'Ideal Gas Law: PV = nRT'
+        }
     }
-  }
 });
-
-Chart.register(Colors)
--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer({alpha: true});
 renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+document.getElementById('simulation').appendChild(renderer.domElement);
 new OrbitControls(camera, renderer.domElement)
 
 const geometry = new THREE.BoxGeometry(25, 25, 25);
